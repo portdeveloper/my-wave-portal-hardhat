853f2819be93fba715e777e326c94154867d4138
abydenes <schoolreef2@protonmail.com>
2023-03-14 21:39:15 +0300

    remove console logs in WavePortal.sol

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,15 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
     mapping(address => uint) public waves;
     mapping(address => string[]) public waveMessages;
-
     mapping(address => uint256) public messageTimes;
-
     event Wave(address _from, string _message);
 
     function wave(string memory _message) public {
@@ -19,10 +16,8 @@ contract WavePortal {
         );
         waves[msg.sender] += 1;
         waveMessages[msg.sender].push(_message);
-        console.log(_message);
         totalWaves += 1;
         messageTimes[msg.sender] = block.timestamp;
-        console.log("%s has waved!", msg.sender);
         emit Wave(msg.sender, _message);
     }
 
@@ -33,7 +28,6 @@ contract WavePortal {
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
